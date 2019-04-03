e24b8b676729747fa93bc20bac9ce9caef13122d
Manoj K R <demonmanoj@gmail.com>
2019-04-03 11:11:27 +0530

    Initial Commit

--- src/table.py
+++ src/table.py
@@ -1,15 +1,34 @@
+import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10,
+          'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
 class Players():
     def __init__(self,assumption_win,jokers_on_hand = 0,round_wins = 0):
         self.hand = []
         self.assumption_win = assumption_win
         self.jokers_on_hand = jokers_on_hand
-        self.round_wins = round_wins
 
 class Deck():
-    pass
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank,values[rank]))
+        self.shuffle()
 class Card():
-    pass
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
 class Round():
-    pass
+    def __init__(self):
+        self.players = []
+        self.deck = Deck()
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
 class Game():
+    round_wins = []
     pass
