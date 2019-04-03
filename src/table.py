fda15e17399c48466a06ef722a1ac01c63a34332
Manoj K R <demonmanoj@gmail.com>
2019-04-03 11:03:33 +0530

    initCommit

---
+++ src/table.py
@@ -0,0 +1,15 @@
+class Players():
+    def __init__(self,assumption_win,jokers_on_hand = 0,round_wins = 0):
+        self.hand = []
+        self.assumption_win = assumption_win
+        self.jokers_on_hand = jokers_on_hand
+        self.round_wins = round_wins
+
+class Deck():
+    pass
+class Card():
+    pass
+class Round():
+    pass
+class Game():
+    pass
