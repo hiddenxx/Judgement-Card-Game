38022f38ee8207ac61da3a85e3fe42c7e04fff10
Manoj K R <demonmanoj@gmail.com>
2019-04-03 11:43:34 +0530

    Initial Commit

--- src/table.py
+++ src/table.py
@@ -31,6 +31,9 @@ class Round():
         self.deck = deck
         self.round_joker = round_joker # This comes from the Players turn.
 
+    def get_round_joker(self):
+        random.shuffle(self.deck)
+        return self.deck.pop()
     def deal(self):
         single_card = self.deck.pop()
         return single_card
@@ -41,7 +44,7 @@ class Game():
         self.round_wins = []
         self.main_joker = main_joker
 
-    def get_main_joker(self):
+    def get_main_joker(self): # This is given by Dealer
         tSuits = random.shuffle(suits)
         return tSuits.pop()
 
